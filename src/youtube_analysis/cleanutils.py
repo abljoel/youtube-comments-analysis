279c8959036c07c0a13e38f4a28c3761b5792015
joelabl <82639814+joelabl@users.noreply.github.com>
2023-11-30 13:01:13 +0100

    Add docstrings in cleanutils.py module

--- src/youtube_analysis/cleanutils.py
+++ src/youtube_analysis/cleanutils.py
@@ -6,11 +6,13 @@ from nltk.stem.wordnet import WordNetLemmatizer
 
 
 def remove_html_tags(text):
+    """Removes HTML tags from text."""
     soup = BeautifulSoup(text, "html.parser")
     return soup.get_text()
 
 
 def translate_emojis(text):
+    """Translates Unicode emojis in text."""
     tokenized_text = word_tokenize(text)
     for emo in UNICODE_EMOJI:
         if emo in tokenized_text:
@@ -22,6 +24,7 @@ def translate_emojis(text):
 
 
 def translate_emoticons(text):
+    """Translates emoticons in text."""
     new_text = text
     for ticon in EMOTICONS_EMO:
         if ticon in new_text:
@@ -30,6 +33,7 @@ def translate_emoticons(text):
 
 
 def filter_text_noise(text):
+    """Removes non-alphabetic characters from text."""
     next_text = text
     for e in next_text:
         if not e.isalpha():
@@ -38,6 +42,7 @@ def filter_text_noise(text):
 
 
 def filter_stopwords(text):
+    """Removes stopwords from text."""
     stop_words = set(stopwords.words("english"))
     tokenized_text = word_tokenize(text)
     filtered_text = []
@@ -48,6 +53,7 @@ def filter_stopwords(text):
 
 
 def lemmatize_text(text):
+    """Lemmatizes words in text."""
     lemmatizer = WordNetLemmatizer()
     tokenized_text = word_tokenize(text)
     return " ".join([lemmatizer.lemmatize(w) for w in tokenized_text])
