7590b45288d1c21eca690d5f6c9e2a829fc0faeb
joelabl <82639814+joelabl@users.noreply.github.com>
2023-11-30 12:35:53 +0100

    Improve docstrings related to data fetching process

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -1,4 +1,25 @@
-"""Data Collection Module"""
+"""
+YouTube Comments Data Collection Script
+
+This module provides functions for fetching and collecting YouTube comments data
+from a specified video using the YouTube API. It uses the 'googleapiclient.discovery'
+library for building the API service and 'pandas' for handling data.
+
+Functions:
+    - fetch_youtube_comments: Fetches YouTube comments for a given video.
+    - fetch_batch_data_from_video: Returns a DataFrame of YouTube comments data.
+    - main: Collects YouTube comments data and saves it to a specified output file.
+
+Note:
+    To use the functions in this module, ensure you have the necessary API credentials,
+    including a valid YouTube API key, and have the required libraries installed 
+    (e.g., 'pandas', 'google-api-python-client').
+
+Usage:
+    Run this module as a script to collect YouTube comments data from a specified video.
+    Command Line Example:
+        python data_collecton.py --video_id YOUR_VIDEO_ID --output_file OUTPUT_FILE_PATH
+"""
 import logging
 import os
 from dotenv import load_dotenv
@@ -18,7 +39,30 @@ except:
 
 
 def fetch_youtube_comments(target, video, results, service, version, key):
-    """Returns raw comments from Youtube video with Youtube API"""
+    """
+    Fetches YouTube comments for a given video.
+
+    Args:
+        target (str): The part parameter specifies a comma-separated list
+                      of commentThread resource properties.
+        video (str): The ID of the video for which the comments are fetched.
+        results (int): The maximum number of comments to retrieve.
+        service (str): The API service to use for fetching comments.
+        version (str): The API version to use.
+        key (str): The API key for authentication.
+
+    Returns:
+        list: A list containing information about each comment, including
+              author name, publication date, update date, like count,
+              and comment text.
+
+    Raises:
+        Any exceptions raised during the API request.
+
+    Note:
+        This function uses the YouTube API to fetch comments and may require
+        appropriate API credentials.
+    """
     youtube = googleapiclient.discovery.build(service, version, developerKey=key)
 
     request = youtube.commentThreads().list(
@@ -39,7 +83,7 @@ def fetch_youtube_comments(target, video, results, service, version, key):
                     comment["textDisplay"],
                 ]
             )
-        # Check if there are more comments and continue iterating
+
         if "nextPageToken" in response:
             response = (
                 youtube.commentThreads()
@@ -58,7 +102,28 @@ def fetch_youtube_comments(target, video, results, service, version, key):
 
 
 def fetch_batch_data_from_video(vid, limit=3000):
-    """Returns a dataframe of Youtube data"""
+    """
+    Returns a DataFrame of YouTube data.
+
+    Args:
+        vid (str): The YouTube video ID for which comments data is collected.
+        limit (int, optional): The maximum number of comments to retrieve. Defaults to 3000.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing YouTube comments data with columns:
+            - 'author': Author name of the comment.
+            - 'published_at': Publication date of the comment.
+            - 'updated_at': Update date of the comment.
+            - 'likes': Number of likes on the comment.
+            - 'text': Text content of the comment.
+
+    Raises:
+        googleapiclient.errors.HttpError: If an HTTP error occurs during the API request.
+
+    Note:
+        This function utilizes the 'fetch_youtube_comments' function to retrieve comments
+        and creates a DataFrame with specified column names.
+    """
     api_service_name = "youtube"
     api_version = "v3"
     api_key = YOUTUBE_API_KEY
--- src/youtube_analysis/data.py
+++ src/youtube_analysis/data.py
@@ -1,40 +1,93 @@
+"""
+YouTube API Module
+
+This module provides a function to interact with the YouTube API for fetching comments
+from a specified video. It uses the 'googleapiclient.discovery' library for building
+the API service.
+
+Functions:
+    - fetch_youtube_comments: Fetches YouTube comments for a given video.
+
+Note:
+    To use the functions in this module, ensure you have the necessary API credentials,
+    including a valid API key.
+
+Example:
+    # Import the module
+    import data
+
+    # Call the fetch_youtube_comments function
+    comments = data.fetch_youtube_comments(
+        target='snippet',
+        video='your_video_id',
+        results=10,
+        service='youtube',
+        version='v3',
+        key='your_api_key'
+    )
+"""
+
 import googleapiclient.discovery
 
 
 def fetch_youtube_comments(target, video, results, service, version, key):
-    youtube = googleapiclient.discovery.build(
-        service,
-        version,
-        developerKey=key
-    )
-    
+    """
+    Fetches YouTube comments for a given video.
+
+    Args:
+        target (str): The part parameter specifies a comma-separated list
+                      of commentThread resource properties.
+        video (str): The ID of the video for which the comments are fetched.
+        results (int): The maximum number of comments to retrieve.
+        service (str): The API service to use for fetching comments.
+        version (str): The API version to use.
+        key (str): The API key for authentication.
+
+    Returns:
+        list: A list containing information about each comment, including
+              author name, publication date, update date, like count,
+              and comment text.
+
+    Raises:
+        Any exceptions raised during the API request.
+
+    Note:
+        This function uses the YouTube API to fetch comments and may require
+        appropriate API credentials.
+    """
+    youtube = googleapiclient.discovery.build(service, version, developerKey=key)
+
     request = youtube.commentThreads().list(
-        part=target,
-        videoId=video,
-        maxResults=results
+        part=target, videoId=video, maxResults=results
     )
-    
+
     response = request.execute()
     comments = []
-    while response:  
-        for item in response['items']:
-            comment = item['snippet']['topLevelComment']['snippet']
-            comments.append([
-                comment['authorDisplayName'],
-                comment['publishedAt'],
-                comment['updatedAt'],
-                comment['likeCount'],
-                comment['textDisplay']
-            ])
-        # Check if there are more comments and continue iterating  
-        if 'nextPageToken' in response:  
-            response = youtube.commentThreads().list(  
-                part=target,  
-                videoId=video,  
-                textFormat='plainText',  
-                pageToken=response['nextPageToken']  
-            ).execute()  
-        else:  
+    while response:
+        for item in response["items"]:
+            comment = item["snippet"]["topLevelComment"]["snippet"]
+            comments.append(
+                [
+                    comment["authorDisplayName"],
+                    comment["publishedAt"],
+                    comment["updatedAt"],
+                    comment["likeCount"],
+                    comment["textDisplay"],
+                ]
+            )
+
+        if "nextPageToken" in response:
+            response = (
+                youtube.commentThreads()
+                .list(
+                    part=target,
+                    videoId=video,
+                    textFormat="plainText",
+                    pageToken=response["nextPageToken"],
+                )
+                .execute()
+            )
+        else:
             break
-            
+
     return comments
