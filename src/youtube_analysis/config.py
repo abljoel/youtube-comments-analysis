f2dfa19e002b863914e289eb8551329cb86ea9db
joelabl <82639814+joelabl@users.noreply.github.com>
2023-11-30 12:54:54 +0100

    Add docstring for config.py module

--- src/youtube_analysis/config.py
+++ src/youtube_analysis/config.py
@@ -1,3 +1,22 @@
+"""
+API Key Configuration Module
+
+This module loads key-value pairs from a '.env' file located in the current directory,
+specifically retrieving the YouTube API key.
+
+Functions:
+    - load_dotenv: Loads key-value pairs from a '.env' file.
+
+Variables:
+    - YOUTUBE_API_KEY (str): YouTube API key loaded from the environment variables.
+
+Raises:
+    - Exception: Raised if the '.env' file is not found or does not contain the required 
+      YouTube API key.
+
+Note:
+    To use this module, ensure you have a '.env' file in the project root with the YouTube API key.
+"""
 import os
 from dotenv import load_dotenv
 
